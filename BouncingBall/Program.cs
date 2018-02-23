44f1ee8bf732a9d094ad34a984a43a82e73a0af1
michael-cowan <mcowan92@gmail.com>
2018-02-23 00:00:12 -0500

    commented out 'setTextRendering(False)'

--- BouncingBall/Program.cs
+++ BouncingBall/Program.cs
@@ -12,7 +12,7 @@ namespace BouncingBall
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GameWindow());
         }
     }
